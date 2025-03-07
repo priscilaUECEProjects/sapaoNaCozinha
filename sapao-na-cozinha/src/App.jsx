d6833dd365b021517fd6c2a51e60eac8d34b7165
http-riscila <prigadelha10@gmail.com>
2025-03-07 10:29:21 -0300

    feat:paginaInicial

--- sapao-na-cozinha/src/App.jsx
+++ sapao-na-cozinha/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import PaginaInicial from './components/PaginaInicial';
 
 function App() {
 
 
   return (
-    <div>
-      
+    <div className='flex items-center justify-center min-h-screen bg-[#e7e5e4]'>
+      <PaginaInicial/>
     </div>
   )
 }
---
+++ sapao-na-cozinha/src/components/PaginaInicial.jsx
@@ -0,0 +1,8 @@
+export default function PaginaInicial() {
+  return (
+    <div className='text-center font-bold text-gray-700'>
+      <h1 className="text-5xl p-10">Bem vindo, ______! </h1>
+      <p className="text-2xl">Guarde, organize e calcule suas receitas incríveis de forma prática e acessível. <br/>Sua cozinha, suas regras!</p>
+    </div>
+  )
+}
