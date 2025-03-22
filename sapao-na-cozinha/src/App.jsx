e83fdb1a150316e8261b2899f77dec29972c7fb0
Jessica Cristina da Silva Souza <136021045+jessicacristinass@users.noreply.github.com>
2025-03-22 20:29:33 -0300

    Update App.jsx
    Adição da rota privada e reorganização das rotas

--- sapao-na-cozinha/src/App.jsx
+++ sapao-na-cozinha/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Routes, Route } from "react-router-dom";
 import MainPage from './components/pages/mainPage/MainPage.jsx';
-
+import PrivateRoute from "./components/pages/PrivateRoute.jsx"
 import PaginaCadastro from './components/pages/PaginaCadastro.jsx';
 import PaginaLogin from './components/pages/PaginaLogin.jsx';
 
@@ -9,9 +9,9 @@ function App() {
   return (
     <>
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<PaginaLogin />} />
+          <Route path="/" element={<PaginaLogin />} />
           <Route path="/cadastro" element={<PaginaCadastro />} />
+          <Route path="/SapaoNaCozinha" element={<PrivateRoute><MainPage /></PrivateRoute>} />
         </Routes>
     </>
   );
