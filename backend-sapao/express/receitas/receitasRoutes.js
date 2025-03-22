11a0d70ded831f8eaec9fc27ac80b6e24f58c068
Jessica Cristina da Silva Souza <136021045+jessicacristinass@users.noreply.github.com>
2025-03-22 20:20:17 -0300

    Upload da pasta express
    Pasta que contes controllers e routes do login e receitas

---
+++ backend-sapao/express/receitas/receitasRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const receitasControllers = require('./receitasControllers.js');
+
+router.post('/', receitasControllers.getReceitasFromUser);
+
+router.delete('/:ID', receitasControllers.deleteReceitaFromUser);
+
+router.put('/:ID', receitasControllers.UpdateReceita);
+
+router.delete('/ingrediente/:ID_IR', receitasControllers.DeleteIngredienteFromReceita);
+
+router.post('/opcoes', receitasControllers.getIngredientesDisponiveis)
+
+module.exports = router;
