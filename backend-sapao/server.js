bd78af1f2d7d7aff2d3166d0c5f1d435197c6651
Jessica Cristina da Silva Souza <136021045+jessicacristinass@users.noreply.github.com>
2025-03-25 11:28:46 -0300

    Update server.js
    Remoção do antigo cadastro de receita

--- backend-sapao/server.js
+++ backend-sapao/server.js
@@ -41,23 +41,6 @@ app.post("/USUARIOS", async (req, res) => {
     }
 });
 
-//Página Cadastrar: Ingredientes
-app.post("/INGREDIENTES", async (req, res) => {
-    try{
-        const { nome, preco, gramatura } = req.body;
-
-        const novoIngrediente = await prisma.iNGREDIENTES.create({
-            data: { NOME: nome, PRECO_UNITARIO: preco, GRAMATURA_UNITARIA: gramatura },
-        });
-
-        res.status(201).json(novoIngrediente);
-    } catch (error) {
-        res
-            .status(400)
-            .json({error: "Erro ao criar ingrediente", details: error.message});
-    }
-});
-
 const PORT = 3000;
 app.listen(PORT, () =>
   console.log(`🚀 Servidor rodando em http://localhost:${PORT}`)
